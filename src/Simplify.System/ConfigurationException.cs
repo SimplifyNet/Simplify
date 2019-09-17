aba216385505fa5625001b44140c3ff56fc43562
Alexanderius <alexander.krylkov@gmail.com>
2019-09-17 13:14:44 +0600

    ConfigurationException added

---
+++ src/Simplify.System/ConfigurationException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Simplify.System
+{
+	/// <summary>
+	/// Provides configuration related errors
+	/// </summary>
+	/// <seealso cref="Exception" />
+	public class ConfigurationException : Exception
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ConfigurationException" /> class.
+		/// </summary>
+		/// <param name="message">The message that describes the error.</param>
+		public ConfigurationException(string message) : base(message)
+		{
+		}
+	}
+}
