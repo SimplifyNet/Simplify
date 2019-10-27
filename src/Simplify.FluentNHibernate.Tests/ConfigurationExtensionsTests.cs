0883440087b22cc0624e9bf569524daeb6ed3b55
Alexanderius <alexander.krylkov@gmail.com>
2019-10-27 13:11:09 +0600

    Tests  names refactored

--- src/Simplify.FluentNHibernate.Tests/ConfigurationExtensionsTests.cs
+++ src/Simplify.FluentNHibernate.Tests/ConfigurationExtensionsTests.cs
@@ -21,19 +21,28 @@ namespace Simplify.FluentNHibernate.Tests
 		}
 
 		[Test]
-		public void ConfigurationExtensions_InitializeFromConfigOracleClient_InitializedCorrectly()
+		public void InitializeFromConfigOracleClient_CorrectConfig_NoExceptions()
 		{
+			// Act
+
 			Fluently.Configure().InitializeFromConfigOracleClient();
 			Fluently.Configure().InitializeFromConfigOracleClient(ConfigSectionName, c => c.Dialect<Oracle10gDialect>());
 
 			Fluently.Configure().InitializeFromConfigOracleClient(_configuration);
+		}
 
+		[Test]
+		public void InitializeFromConfigOracleClient_SectionNotFound_DatabaseConnectionConfigurationException()
+		{
+			// Act
 			Assert.Throws<DatabaseConnectionConfigurationException>(() => Fluently.Configure().InitializeFromConfigOracleClient("foo"));
 		}
 
 		[Test]
-		public void ConfigurationExtensions_InitializeFromConfigOracleOdpNetNative_InitializedCorrectly()
+		public void InitializeFromConfigOracleOdpNetNative_CorrectConfig_NoException()
 		{
+			// Act
+
 			Fluently.Configure().InitializeFromConfigOracleOdpNetNative();
 			Fluently.Configure().InitializeFromConfigOracleOdpNetNative(ConfigSectionName, c => c.Dialect<Oracle10gDialect>());
 
@@ -41,8 +50,10 @@ namespace Simplify.FluentNHibernate.Tests
 		}
 
 		[Test]
-		public void ConfigurationExtensions_InitializeFromConfigOracleOdpNet_InitializedCorrectly()
+		public void InitializeFromConfigOracleOdpNet_CorrectConfig_NoExceptions()
 		{
+			// Act
+
 			Fluently.Configure().InitializeFromConfigOracleOdpNet();
 			Fluently.Configure().InitializeFromConfigOracleOdpNet(ConfigSectionName, c => c.Dialect<Oracle10gDialect>());
 
@@ -50,8 +61,10 @@ namespace Simplify.FluentNHibernate.Tests
 		}
 
 		[Test]
-		public void ConfigurationExtensions_InitializeFromConfigMySql_InitializedCorrectly()
+		public void InitializeFromConfigMySql_CorrectConfig_NoExceptions()
 		{
+			// Act
+
 			Fluently.Configure().InitializeFromConfigMySql();
 			Fluently.Configure().InitializeFromConfigMySql(ConfigSectionName, c => c.Dialect<MySQL5Dialect>());
 
@@ -59,8 +72,10 @@ namespace Simplify.FluentNHibernate.Tests
 		}
 
 		[Test]
-		public void ConfigurationExtensions_InitializeFromConfigMsSql_InitializedCorrectly()
+		public void InitializeFromConfigMsSql_CorrectConfig_NoExceptions()
 		{
+			// Act
+
 			Fluently.Configure().InitializeFromConfigMsSql();
 			Fluently.Configure().InitializeFromConfigMsSql(ConfigSectionName, c => c.Dialect<MsSql2012Dialect>());
 
@@ -68,8 +83,10 @@ namespace Simplify.FluentNHibernate.Tests
 		}
 
 		[Test]
-		public void ConfigurationExtensions_InitializeFromConfig_InitializedCorrectly()
+		public void InitializeFromConfigSqLiteInMemory_CorrectConfig_NoExceptions()
 		{
+			// Act
+
 			Fluently.Configure().InitializeFromConfigSqLiteInMemory(true);
 			Fluently.Configure().InitializeFromConfigSqLiteInMemory(true, c => c.Dialect<SQLiteDialect>());
 		}
