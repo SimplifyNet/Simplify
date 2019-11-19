ada221b0e485a2c83cedec1c4a3fd6b09457720a
Alexanderius <alexander.krylkov@gmail.com>
2019-11-19 22:50:30 +0600

    Localization tests

--- src/Simplify.Templates.Tests/TemplateBuilderFromFileTests.cs
+++ src/Simplify.Templates.Tests/TemplateBuilderFromFileTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
 using NUnit.Framework;
 
 namespace Simplify.Templates.Tests
@@ -7,6 +9,7 @@ namespace Simplify.Templates.Tests
 	public class TemplateBuilderFromFileTests
 	{
 		private const string LocalTestFilePath = "TestTemplates/Local/TestFile.txt";
+		private const string LocalizationTestFilePath = "TestTemplates/Local/LocalizationTest.tpl";
 
 		[SetUp]
 		public void Initialize()
@@ -35,6 +38,62 @@ namespace Simplify.Templates.Tests
 
 			// Assert
 			Assert.AreEqual("test", tpl.Get());
+		}
+
+		[Test]
+		public void Build_FromFileLocalizableDifferentFromBase_LocalizableLoadedWithBaseReplacements()
+		{
+			// Act
+			var tpl = TemplateBuilder
+				.FromFile(LocalizationTestFilePath)
+				.Localizable("ru")
+				.Build();
+
+			// Assert
+			Assert.AreEqual("текст1 text2", tpl.Get());
+		}
+
+		[Test]
+		public async Task BuildAsync_FromFileLocalizableDifferentFromBase_LocalizableLoadedWithBaseReplacements()
+		{
+			// Act
+			var tpl = await TemplateBuilder
+				.FromFile(LocalizationTestFilePath)
+				.Localizable("ru")
+				.BuildAsync();
+
+			// Assert
+			Assert.AreEqual("текст1 text2", tpl.Get());
+		}
+
+		[Test]
+		public void Build_FromFileLocalizableFromCurrentThreadLanguageDifferentFromBase_LocalizableLoadedWithBaseReplacements()
+		{
+			Thread.CurrentThread.CurrentCulture = new CultureInfo("ru");
+
+			// Act
+			var tpl = TemplateBuilder
+				.FromFile(LocalizationTestFilePath)
+				.LocalizableFromCurrentThreadLanguage()
+				.Build();
+
+			// Assert
+			Assert.AreEqual("текст1 text2", tpl.Get());
+		}
+
+		[Test]
+		public async Task BuildAsync_FromFileLocalizableFromCurrentThreadLanguageDifferentFromBase_LocalizableLoadedWithBaseReplacements()
+		{
+			Thread.CurrentThread.CurrentCulture = new CultureInfo("ru");
+
+			// Act
+			var tpl = await TemplateBuilder
+				.FromFile(LocalizationTestFilePath)
+				.LocalizableFromCurrentThreadLanguage()
+				.BuildAsync();
+
+			// Assert
+			Assert.AreEqual("текст1 text2", tpl.Get());
 		}
 	}
 }
