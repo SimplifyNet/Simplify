d7fd40a476b4b97f15414c7b782e755aece8f063
Alexanderius <alexander.krylkov@gmail.com>
2019-12-07 19:51:51 +0600

    Set and Add methods tests

--- src/Simplify.Templates/TemplateSetExtensions.cs
+++ src/Simplify.Templates/TemplateSetExtensions.cs
@@ -8,15 +8,15 @@ namespace Simplify.Templates
 	public static class TemplateSetExtensions
 	{
 		/// <summary>
-		/// Set template variable value (all occurrences will be replaced)
+		/// Set template variable value with text from template (all occurrences will be replaced)
 		/// </summary>
-		/// <param name="tpl">The TPL.</param>
+		/// <param name="tpl">Value to set</param>
 		/// <param name="variableName">Variable name</param>
-		/// <param name="value">Value to set</param>
+		/// <param name="template">The template.</param>
 		/// <returns></returns>
-		public static ITemplate Set(this ITemplate tpl, string variableName, int value)
+		public static ITemplate Set(this ITemplate tpl, string variableName, ITemplate template)
 		{
-			return tpl.Set(variableName, value.ToString(CultureInfo.InvariantCulture));
+			return tpl.Set(variableName, template?.Get());
 		}
 
 		/// <summary>
@@ -38,7 +38,19 @@ namespace Simplify.Templates
 		/// <param name="variableName">Variable name</param>
 		/// <param name="value">Value to set</param>
 		/// <returns></returns>
-		public static ITemplate Set(this ITemplate tpl, string variableName, double value)
+		public static ITemplate Set(this ITemplate tpl, string variableName, int value)
+		{
+			return tpl.Set(variableName, value.ToString(CultureInfo.InvariantCulture));
+		}
+
+		/// <summary>
+		/// Set template variable value (all occurrences will be replaced)
+		/// </summary>
+		/// <param name="tpl">The TPL.</param>
+		/// <param name="variableName">Variable name</param>
+		/// <param name="value">Value to set</param>
+		/// <returns></returns>
+		public static ITemplate Set(this ITemplate tpl, string variableName, long value)
 		{
 			return tpl.Set(variableName, value.ToString(CultureInfo.InvariantCulture));
 		}
@@ -62,21 +74,9 @@ namespace Simplify.Templates
 		/// <param name="variableName">Variable name</param>
 		/// <param name="value">Value to set</param>
 		/// <returns></returns>
-		public static ITemplate Set(this ITemplate tpl, string variableName, long value)
+		public static ITemplate Set(this ITemplate tpl, string variableName, double value)
 		{
 			return tpl.Set(variableName, value.ToString(CultureInfo.InvariantCulture));
-		}
-
-		/// <summary>
-		/// Set template variable value with text from template (all occurrences will be replaced)
-		/// </summary>
-		/// <param name="tpl">Value to set</param>
-		/// <param name="variableName">Variable name</param>
-		/// <param name="template">The template.</param>
-		/// <returns></returns>
-		public static ITemplate Set(this ITemplate tpl, string variableName, ITemplate template)
-		{
-			return tpl.Set(variableName, template?.Get());
 		}
 	}
 }
