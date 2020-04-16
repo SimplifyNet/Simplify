a6f6b82a6254a9a8205e4261c8261027785c8480
Dinmukhamed <dinmukhamed.09@gmail.com>
2020-04-17 00:32:01 +0300

    Fixed test for IndexesForForeignKeys

--- src/Simplify.FluentNHibernate.Tests/NHibernateConfigurationExtensionsTests.cs
+++ src/Simplify.FluentNHibernate.Tests/NHibernateConfigurationExtensionsTests.cs
@@ -1,13 +1,13 @@
-﻿using System.IO;
-using System.Linq;
-using System.Text.RegularExpressions;
-using FluentNHibernate.Cfg;
+﻿using FluentNHibernate.Cfg;
 using FluentNHibernate.Conventions.Helpers;
 using NHibernate.Cfg;
 using NHibernate.Tool.hbm2ddl;
 using NUnit.Framework;
 using Simplify.FluentNHibernate.Conventions;
 using Simplify.FluentNHibernate.Tests.Mappings.Accounts;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Simplify.FluentNHibernate.Tests
 {
@@ -44,12 +44,14 @@ namespace Simplify.FluentNHibernate.Tests
 			var matches = Regex.Matches(result, @"IDX\w+");
 			var indexes = (from Match match in matches select match.Value).ToList();
 
-			Assert.AreEqual(5, indexes.Count);
-			Assert.AreEqual("IDX_FK_UsersGroups_UserID", indexes[0]);
-			Assert.AreEqual("IDX_FK_UsersGroups_GroupID", indexes[1]);
-			Assert.AreEqual("IDX_FK_Custom_UsersPrivileges_GroupID", indexes[2]);
-			Assert.AreEqual("IDX_FK_User_OrganizationID", indexes[3]);
-			Assert.AreEqual("IDX_FK_UsersPrivileges_UserID", indexes[4]);
+			Assert.AreEqual(7, indexes.Count);
+			Assert.AreEqual("IDX_FK_Employee_User", indexes[0]);
+			Assert.AreEqual("IDX_FK_UsersGroups_UserID", indexes[1]);
+			Assert.AreEqual("IDX_FK_UsersGroups_GroupID", indexes[2]);
+			Assert.AreEqual("IDX_FK_Custom_UsersPrivileges_GroupID", indexes[3]);
+			Assert.AreEqual("IDX_FK_Traveler_EmployeeID", indexes[4]);
+			Assert.AreEqual("IDX_FK_User_OrganizationID", indexes[5]);
+			Assert.AreEqual("IDX_FK_UsersPrivileges_UserID", indexes[6]);
 		}
 
 		private static FluentConfiguration CreateConfiguration()
