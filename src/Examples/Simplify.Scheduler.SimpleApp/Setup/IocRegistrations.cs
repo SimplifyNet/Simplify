3ee7e5b9aba6806eaeb58fc1d62a0e9f375eac03
Alexanderius <alexander.krylkov@gmail.com>
2020-06-27 18:34:05 +0600

    Simplify.Scheduler simple app

---
+++ src/Examples/Simplify.Scheduler.SimpleApp/Setup/IocRegistrations.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using Simplify.DI;
+
+namespace Simplify.Scheduler.SimpleApp.Setup
+{
+	public static class IocRegistrations
+	{
+		public static IConfiguration Configuration { get; private set; }
+
+		public static IDIContainerProvider Register()
+		{
+			DIContainer.Current.RegisterConfiguration()
+				.Register<PeriodicalProcessor>();
+
+			return DIContainer.Current;
+		}
+
+		private static IDIRegistrator RegisterConfiguration(this IDIRegistrator registrator)
+		{
+			Configuration = new ConfigurationBuilder()
+				.AddJsonFile("appsettings.json", false)
+				.Build();
+
+			registrator.Register(p => Configuration, LifetimeType.Singleton);
+
+			return registrator;
+		}
+	}
+}
