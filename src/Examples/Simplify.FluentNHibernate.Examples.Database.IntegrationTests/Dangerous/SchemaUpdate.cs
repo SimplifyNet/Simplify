91b2f84adb048e70fef868f0f1c0288fd3f51f69
Alexanderius <alexander.krylkov@gmail.com>
2021-01-06 22:44:58 +0600

    Async operation example added
    Schema update (code-first) example added
    Advanced configuration and conventions example added

---
+++ src/Examples/Simplify.FluentNHibernate.Examples.Database.IntegrationTests/Dangerous/SchemaUpdate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using NHibernate.Cfg;
+using NUnit.Framework;
+
+namespace Simplify.FluentNHibernate.Examples.Database.IntegrationTests.Dangerous
+{
+	[TestFixture]
+	[Category("Integration")]
+	public class SchemaUpdate
+	{
+		[Test]
+		public void UpdateSchema()
+		{
+			var cfg = new ConfigurationBuilder()
+				.AddJsonFile("appsettings.json", false)
+				.Build();
+
+			var configuration = new ExampleSessionFactoryBuilder(cfg).CreateConfiguration();
+
+			Configuration config = null;
+			configuration.ExposeConfiguration(c => config = c);
+			configuration.BuildSessionFactory();
+
+			config.CreateIndexesForForeignKeys();
+
+			new NHibernate.Tool.hbm2ddl.SchemaUpdate(config).Execute(true, true);
+		}
+	}
+}
