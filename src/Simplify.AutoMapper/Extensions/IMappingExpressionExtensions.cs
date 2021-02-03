583d542d43740ab3019e621bea23b40ce914fb3f
hardcoder <asan.sultan@chronos.codes>
2021-02-03 15:45:43 +0600

    Updated methods MapTo

--- src/Simplify.AutoMapper/Extensions/IMappingExpressionExtensions.cs
+++ src/Simplify.AutoMapper/Extensions/IMappingExpressionExtensions.cs
@@ -10,52 +10,108 @@ namespace Simplify.AutoMapper.Extensions
 	public static class IMappingExpressionExtensions
 	{
 		/// <summary>
-		/// Map destination member using a source member expression
+		/// Map destination member using destination and source member expressions
 		/// </summary>
-		/// <typeparam name="TSource">Source type</typeparam>
 		/// <typeparam name="TDest">Destination type</typeparam>
+		/// <typeparam name="TSource">Source type</typeparam>
 		/// <param name="mappingExpression">IMappingExpression</param>
 		/// <param name="destinationMember">Destination member</param>
 		/// <param name="sourceMember">Source member</param>
 		/// <param name="memberOptions">Member options</param>
 		/// <returns>Itself</returns>
-		public static IMappingExpression<TSource, TDest> Map<TSource, TDest>(this IMappingExpression<TSource, TDest> mappingExpression,
-			Expression<Func<TDest, object?>>? destinationMember,
+		public static IMappingExpression<TSource, TDest> MapTo<TDest, TSource>(this IMappingExpression<TSource, TDest> mappingExpression,
+			Expression<Func<TDest, object?>> destinationMember,
 			Expression<Func<TSource, object?>>? sourceMember = null,
 			Action<IMemberConfigurationExpression<TSource, TDest, object?>>? memberOptions = null)
 		{
 			if (destinationMember is null)
-				return mappingExpression;
+				throw new AutoMapperConfigurationException("Destination member expression cannot be null");
 
 			mappingExpression.ForMember(destinationMember, o =>
 			{
-				o.MapFrom(sourceMember ?? (x => x));
+				o.MapFrom(sourceMember ?? (s => s));
 				memberOptions?.Invoke(o);
 			});
 			return mappingExpression;
 		}
 
 		/// <summary>
-		/// Map destination member using a source member's path
+		/// Map destination member using name and source member expressions
 		/// </summary>
-		/// <typeparam name="TSource">Source type</typeparam>
 		/// <typeparam name="TDest">Destination type</typeparam>
+		/// <typeparam name="TSource">Source type</typeparam>
 		/// <param name="mappingExpression">IMappingExpression</param>
 		/// <param name="destinationMember">Destination member</param>
-		/// <param name="sourceMembersPath">Property name referencing the source member to map against. Or a dot separated member path.</param>
+		/// <param name="sourceMember">Source member</param>
 		/// <param name="memberOptions">Member options</param>
 		/// <returns>Itself</returns>
-		public static IMappingExpression<TSource, TDest> Map<TSource, TDest>(this IMappingExpression<TSource, TDest> mappingExpression,
-			Expression<Func<TDest, object?>>? destinationMember,
-			string sourceMembersPath,
+		public static IMappingExpression<TSource, TDest> MapTo<TDest, TSource>(this IMappingExpression<TSource, TDest> mappingExpression,
+			string destinationMember,
+			Expression<Func<TSource, object?>>? sourceMember = null,
 			Action<IMemberConfigurationExpression<TSource, TDest, object?>>? memberOptions = null)
 		{
-			if (destinationMember is null)
-				return mappingExpression;
+			if (string.IsNullOrWhiteSpace(destinationMember))
+				throw new AutoMapperConfigurationException("Destination member name cannot be null or empty");
 
 			mappingExpression.ForMember(destinationMember, o =>
 			{
-				o.MapFrom(sourceMembersPath);
+				o.MapFrom(sourceMember ?? (s => s));
+				memberOptions?.Invoke(o);
+			});
+			return mappingExpression;
+		}
+
+		/// <summary>
+		/// Map destination member using expression and source member's path
+		/// </summary>
+		/// <typeparam name="TDest">Destination type</typeparam>
+		/// <typeparam name="TSource">Source type</typeparam>
+		/// <param name="mappingExpression">IMappingExpression</param>
+		/// <param name="destinationMember">Destination member</param>
+		/// <param name="sourceMemberPath">Property name referencing the source member to map against. Or a dot separated member path.</param>
+		/// <param name="memberOptions">Member options</param>
+		/// <returns>Itself</returns>
+		public static IMappingExpression<TSource, TDest> MapTo<TDest, TSource>(this IMappingExpression<TSource, TDest> mappingExpression,
+			Expression<Func<TDest, object?>> destinationMember,
+			string sourceMemberPath,
+			Action<IMemberConfigurationExpression<TSource, TDest, object?>>? memberOptions = null)
+		{
+			if (destinationMember is null)
+				throw new AutoMapperConfigurationException("Destination member expression cannot be null");
+			if (string.IsNullOrWhiteSpace(sourceMemberPath))
+				throw new AutoMapperConfigurationException("Source member path cannot be null or empty");
+
+			mappingExpression.ForMember(destinationMember, o =>
+			{
+				o.MapFrom(sourceMemberPath);
+				memberOptions?.Invoke(o);
+			});
+			return mappingExpression;
+		}
+
+		/// <summary>
+		/// Map destination member using name and source member's path
+		/// </summary>
+		/// <typeparam name="TDest">Destination type</typeparam>
+		/// <typeparam name="TSource">Source type</typeparam>
+		/// <param name="mappingExpression">IMappingExpression</param>
+		/// <param name="destinationMember">Destination member</param>
+		/// <param name="sourceMemberPath">Property name referencing the source member to map against. Or a dot separated member path.</param>
+		/// <param name="memberOptions">Member options</param>
+		/// <returns>Itself</returns>
+		public static IMappingExpression<TSource, TDest> MapTo<TDest, TSource>(this IMappingExpression<TSource, TDest> mappingExpression,
+			string destinationMember,
+			string sourceMemberPath,
+			Action<IMemberConfigurationExpression<TSource, TDest, object?>>? memberOptions = null)
+		{
+			if (string.IsNullOrWhiteSpace(destinationMember))
+				throw new AutoMapperConfigurationException("Destination member name cannot be null or empty");
+			if (string.IsNullOrWhiteSpace(sourceMemberPath))
+				throw new AutoMapperConfigurationException("Source member path cannot be null or empty");
+
+			mappingExpression.ForMember(destinationMember, o =>
+			{
+				o.MapFrom(sourceMemberPath);
 				memberOptions?.Invoke(o);
 			});
 			return mappingExpression;
