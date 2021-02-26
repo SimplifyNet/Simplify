4c59f7c8e52b8413dc2015fc1b214fe1ee284a2a
Alexanderius <alexander.krylkov@gmail.com>
2021-02-26 16:44:14 +0600

    Setting isolation level

--- src/Simplify.Repository.FluentNHibernate/SimplifyDIRegistratorExtensions.cs
+++ src/Simplify.Repository.FluentNHibernate/SimplifyDIRegistratorExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Simplify.DI;
+﻿using System.Data;
+using Simplify.DI;
 
 namespace Simplify.Repository.FluentNHibernate
 {
@@ -8,31 +9,33 @@ namespace Simplify.Repository.FluentNHibernate
 	public static class SimplifyDIRegistratorExtensions
 	{
 		/// <summary>
-		/// Registers the scoped GenericRepository&lt;T&gt; and IGenericRepository&lt;T&gt; as TransactGenericRepository&lt;T&gt; transact repository.
-		/// </summary>
-		/// <typeparam name="T"></typeparam>
-		/// <typeparam name="TUnitOfWork">The type of the unit of work.</typeparam>
-		/// <param name="registrator">The registrator.</param>
-		/// <returns></returns>
-		public static IDIRegistrator RegisterTransactRepository<T, TUnitOfWork>(this IDIRegistrator registrator)
-			where T : class
-			where TUnitOfWork : ITransactUnitOfWork
-			=> registrator.Register<GenericRepository<T>>()
-				.Register<IGenericRepository<T>>(r =>
-					new TransactGenericRepository<T>(r.Resolve<GenericRepository<T>>(), r.Resolve<TUnitOfWork>()));
-
-		/// <summary>
 		/// Registers the scoped StatelessGenericRepository&lt;T&gt; and IGenericRepository&lt;T&gt; as TransactGenericRepository&lt;T&gt; transact repository.
 		/// </summary>
 		/// <typeparam name="T"></typeparam>
 		/// <typeparam name="TUnitOfWork">The type of the unit of work.</typeparam>
 		/// <param name="registrator">The registrator.</param>
+		/// <param name="isolationLevel">The isolation level.</param>
 		/// <returns></returns>
-		public static IDIRegistrator RegisterStatelessTransactRepository<T, TUnitOfWork>(this IDIRegistrator registrator)
+		public static IDIRegistrator RegisterStatelessTransactRepository<T, TUnitOfWork>(this IDIRegistrator registrator, IsolationLevel isolationLevel = IsolationLevel.ReadCommitted)
 			where T : class
 			where TUnitOfWork : ITransactUnitOfWork
 			=> registrator.Register<StatelessGenericRepository<T>>()
 				.Register<IGenericRepository<T>>(r =>
-					new TransactGenericRepository<T>(r.Resolve<StatelessGenericRepository<T>>(), r.Resolve<TUnitOfWork>()));
+					new TransactGenericRepository<T>(r.Resolve<StatelessGenericRepository<T>>(), r.Resolve<TUnitOfWork>(), isolationLevel));
+
+		/// <summary>
+		/// Registers the scoped GenericRepository&lt;T&gt; and IGenericRepository&lt;T&gt; as TransactGenericRepository&lt;T&gt; transact repository.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <typeparam name="TUnitOfWork">The type of the unit of work.</typeparam>
+		/// <param name="registrator">The registrator.</param>
+		/// <param name="isolationLevel">The isolation level.</param>
+		/// <returns></returns>
+		public static IDIRegistrator RegisterTransactRepository<T, TUnitOfWork>(this IDIRegistrator registrator, IsolationLevel isolationLevel = IsolationLevel.ReadCommitted)
+			where T : class
+			where TUnitOfWork : ITransactUnitOfWork
+			=> registrator.Register<GenericRepository<T>>()
+				.Register<IGenericRepository<T>>(r =>
+					new TransactGenericRepository<T>(r.Resolve<GenericRepository<T>>(), r.Resolve<TUnitOfWork>(), isolationLevel));
 	}
 }
