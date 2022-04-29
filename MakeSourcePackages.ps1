317a74e1ca0d5ae49f2a15500ee3eecd85e04fbe
Alexanderius <alexander.krylkov@gmail.com>
2022-04-29 17:24:54 +0600

    [fix] path

--- MakeSourcePackages.ps1
+++ MakeSourcePackages.ps1
@@ -13,7 +13,7 @@ foreach ($file in $files)
 
 # Getting packages versions
 
-$xmlVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo("$PSScriptRoot/src/Simplify.Xml/binRelease/netstandard2.0/Simplify.Xml.dll").FileVersion
+$xmlVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo("$PSScriptRoot/src/Simplify.Xml/bin/Release/netstandard2.0/Simplify.Xml.dll").FileVersion
 $stringVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo("$PSScriptRoot/src/Simplify.String/bin/Release/net452/Simplify.String.dll").FileVersion
 $systemVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo("$PSScriptRoot/src/Simplify.System/bin/Release/netstandard2.0/Simplify.System.dll").FileVersion
 
