665a5a6131ecd19411a8fa2632cf3c0bfbb0d1bc
Alexanderius <alexander.krylkov@gmail.com>
2023-07-31 16:27:44 +0600

    [fix] typo

--- src/Simplify.DI/BeginLifetimeScopeEventHandler.cs
+++ src/Simplify.DI/BeginLifetimeScopeEventHandler.cs
@@ -1,7 +1,7 @@
 namespace Simplify.DI;
 
 /// <summary>
-/// Represent beging lifetime scope related events handler
+/// Represent begin lifetime scope related events handler
 /// </summary>
 /// <param name="scope">The lifetime scope.</param>
 public delegate void BeginLifetimeScopeEventHandler(ILifetimeScope scope);
