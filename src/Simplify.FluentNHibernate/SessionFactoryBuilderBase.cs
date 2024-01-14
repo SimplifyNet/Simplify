849f6be4c460990683c65a4e1e2c508c796c0d3b
Alexanderius <alexander.krylkov@gmail.com>
2024-01-14 10:29:00 +0600

    [add] correct session factory dispose

--- src/Simplify.FluentNHibernate/SessionFactoryBuilderBase.cs
+++ src/Simplify.FluentNHibernate/SessionFactoryBuilderBase.cs
@@ -91,5 +91,22 @@ public abstract class SessionFactoryBuilderBase : IDisposable
 	/// <summary>
 	/// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
 	/// </summary>
-	public virtual void Dispose() => Instance?.Dispose();
+	public virtual void Dispose()
+	{
+		Dispose(true);
+		GC.SuppressFinalize(this);
+	}
+
+	/// <summary>
+	/// Releases unmanaged and - optionally - managed resources.
+	/// </summary>
+	/// <param name="disposing"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
+	protected virtual void Dispose(bool disposing)
+	{
+		if (!disposing)
+			return;
+
+		Instance?.Dispose();
+		Instance = null;
+	}
 }
