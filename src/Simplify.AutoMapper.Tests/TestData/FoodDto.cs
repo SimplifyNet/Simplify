cad847fcf61b8071d597163b1224a0c204e5f01c
Alexanderius <alexander.krylkov@gmail.com>
2026-05-26 23:46:39 +0500

    [del] Simplify.AutoMapper since package is not maintained, AutoMapper changed license and better to do manual conversions

--- src/Simplify.AutoMapper.Tests/TestData/FoodDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Simplify.AutoMapper.Tests.TestData;
-
-internal record FoodDto : IFoodDto
-{
-	public int Count { get; init; }
-	public string? Name { get; init; }
-	public string? Source { get; init; }
-	public string? Type { get; init; }
-}
--- src/Simplify.AutoMapper.Tests/TestData/IFoodDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Simplify.AutoMapper.Tests.TestData;
-
-internal interface IFoodDto
-{
-	int Count { get; }
-	string? Name { get; }
-	string? Source { get; }
-	string? Type { get; }
-}
